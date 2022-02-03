c47be0f153b6d130759ee01fe664f6200d28b7c7
Vladislav220294 <vladislav22021994@gmail.com>
2022-02-03 23:51:52 +0300

    add form

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,7 +1,17 @@
 import PropTypes from 'prop-types';
 
-const Filter = () => {
-    return ( <div></div> );
-}
- 
+const Filter = ({ onChange, value }) => {
+  return (
+    <div>
+      <label>
+        Find contacts by name
+        <input type="text" value={value} onChange={onChange} />
+      </label>
+    </div>
+  );
+};
+Filter.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+};
 export default Filter;
